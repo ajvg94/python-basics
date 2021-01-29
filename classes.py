03d21c5631bc7d138c2b7891ced8dbf94309072f
ajvg94 <atiliovg94@gmail.com>
2021-01-29 10:45:37 -0300

    update

--- classes.py
+++ classes.py
@@ -2,14 +2,27 @@
 #Classes
 
 # The simplest form of class definition looks like this:
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
 
-class ClassName:
-    # <statement-1>
-    # .
-    # .
-    # .
-    # <statement-N>
+x = Complex(3.0, -4.5)
+print (x.r +","+ x.i)   #(3.0, -4.5)
 ##--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--##
+class Dog:
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
 
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.add_trick('roll over')
+e.add_trick('play dead')
+print (d.tricks)    # ['roll over']
+print (e.tricks)    # ['play dead']
 ##--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--##
 
--- regular expressions.py
+++ regular expressions.py
@@ -14,7 +14,7 @@ match = re.search(r'word:\w\w\w', str)
 if match:
   print 'found', match.group() ## 'found word:cat'
 else:
-  print 'did not find'
+  print ('did not find')
 ##--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--####--##
 """
 Basic Patterns
